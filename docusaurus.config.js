4e1bee4b3fc71471755005d0d4e64d45dda1ab2b
jillian-maroket <67180770+jillian-maroket@users.noreply.github.com>
2024-04-14 21:57:00 +0800

    Change GitHub URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
+  organizationName: 'jillian-maroket', // Usually your GitHub org/user name.
   projectName: 'portfolio', // Usually your repo name.
 
   onBrokenLinks: 'throw',
@@ -44,14 +44,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/jillian-maroket',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/jillian-maroket/portfolio',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -81,7 +81,7 @@ const config = {
           {to: '/docs/expertise', label: 'Expertise', position: 'left'},
           {to: '/docs/sample-work', label: 'Sample Work', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/jillian-maroket',
             label: 'GitHub',
             position: 'right',
           },
